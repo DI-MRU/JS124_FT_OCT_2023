bac87775581ae2b653d54bf46e5633020159fee1
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2024-01-19 12:50:46 +0400

    feat: add sign in, forget pass, reset pass, home

--- week_09/day_5/food-app/src/app/password-reset/page.jsx
+++ week_09/day_5/food-app/src/app/password-reset/page.jsx
@@ -1,30 +1,32 @@
 "use client";
 
+import { useState } from "react";
+import Link from "next/link";
+
 import { Button, Paper, TextField } from "@mui/material";
 import { createClient } from "@supabase/supabase-js";
-import { useState } from "react";
 
-const Login = () => {
+const PasswordResetPage = () => {
   const supabase = createClient(
     "https://xglwanpkbcvknvwjbovk.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnbHdhbnBrYmN2a252d2pib3ZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUwMzkxNjAsImV4cCI6MjAyMDYxNTE2MH0.WiXWLqfjCg_Gs0mNsjgPBi-qvBoiyhMvmIvchaSelfs"
   );
 
-  const signUp = async (e) => {
+  const [email, setEmail] = useState("");
+
+  const resetPassword = async (e) => {
     e.stopPropagation();
     e.preventDefault();
-    const { data, error } = await supabase.auth.signUp({
-      email: email,
-      password: password,
+
+    const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo: "https://betise.betise.com",
     });
   };
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
   return (
     <>
       <Paper className="w-2/6 mx-auto p-12 absolute top-1/2 left-1/2">
+        <h1>Password Reset</h1>
         <form method="POST">
           <TextField
             className="flex w-3/4 mx-auto"
@@ -34,24 +36,20 @@ const Login = () => {
             variant="standard"
             onChange={(e) => setEmail(e.target.value)}
           />
-          <TextField
-            className="flex w-3/4 mx-auto mt-4"
-            id="password"
-            name="password"
-            label="Password"
-            variant="standard"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
           <div className="flex w-3/4 mx-auto mt-8">
             <Button
               className="ml-auto"
-              variant="contained"
+              variant="outlined"
               type="submit"
-              onClick={(e) => signUp(e)}
+              onClick={(e) => resetPassword(e)}
             >
-              Sign Up
+              Reset password
             </Button>
+            <Link href="/login">
+              <Button className="ml-auto" variant="outlined">
+                Back to Log In
+              </Button>
+            </Link>
           </div>
         </form>
       </Paper>
@@ -59,4 +57,4 @@ const Login = () => {
   );
 };
 
-export { Login, Login as default };
+export { PasswordResetPage, PasswordResetPage as default };
